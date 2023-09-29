bfa77b0553b2d5d6193ae63f40ef3b0005e81038
Kritsana <poddknws@gmail.com>
2023-09-29 18:11:35 +0700

    Adding the navigation bar and enter your email and password

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,5 +127,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_POST = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'poddknws@gmail.com'
-EMAIL_HOST_PASSWORD = 'bcseyvnubyrrwpmz'
+EMAIL_HOST_USER = 'Your Email'
+EMAIL_HOST_PASSWORD = 'Your Password'
